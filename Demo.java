5c0429f27b0b14b8f75da6d1b892805f635ce8bb
RutujaJadhav729 <jadhavrutuja227@gmail.com>
2025-04-03 16:22:55 +0530

    Average of Number

--- Demo.java
+++ Demo.java
@@ -1,5 +1,27 @@
 public class Demo {
+    //Average of Numbers
+    static int average(int... a){
+        //length of the number
+        int numberLength = a.length;
+
+        int sumVariable = 0;
+        for (int sum : a){
+            sumVariable += sum;
+        }
+        return (sumVariable/numberLength);
+    }
+
+    //Sum of the N numbers
+    static double sumNumbers(double... b){
+        double sumOfNumbers = 0;
+        for (double sumNumbers : b){
+            sumOfNumbers += sumNumbers;
+        }
+        return sumOfNumbers;
+    }
     public static void main(String[] args) {
-        System.out.println("This is Java file");
+        System.out.println("Average of the given number is:- "+ average(2,9,0,5,33,45,22,24));
+        System.out.println("Average of the given number is:- "+ average(90,89,44,2,3,2,1,4,89,90,0));
+        System.out.println("Sum of the N numbers:- "+ sumNumbers(92,90.3,98.4,8));
     }
 }
